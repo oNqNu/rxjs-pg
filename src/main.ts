91abb95603954af2c646a0aae2245bc4c9b21682
oNqNu <onanukintaro@gmail.com>
2022-07-28 19:08:24 +0900

    feat: error処理の実装

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { Observable } from 'rxjs';
 const obs: Observable<number> = new Observable((sub) => {
   sub.next(1);
   sub.next(2);
+  sub.error(new Error('error'));
   sub.next(3);
+  sub.next(4);
   sub.complete();
 });
 
-obs.subscribe((v: number) => console.log(v));
+obs.subscribe((v: number | string) => console.log(v));
