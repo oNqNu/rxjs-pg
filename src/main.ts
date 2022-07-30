e03a03ccd793f60e0e1a5c40d436da646563f1d1
oNqNu <onanukintaro@gmail.com>
2022-07-30 22:17:49 +0900

    errorのけた

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 const obs: Observable<number> = new Observable((sub) => {
   sub.next(1);
   sub.next(2);
-  sub.error(new Error('error'));
   sub.next(3);
   sub.next(4);
   sub.complete();
